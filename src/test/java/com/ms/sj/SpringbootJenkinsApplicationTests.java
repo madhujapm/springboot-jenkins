7068ee111594841377b15c5da290ede40da3b081
maadhu2811 <madhujamaadhu2711@gmail.com>
2022-03-27 16:21:35 +0530

    added one more test log

--- src/test/java/com/ms/sj/SpringbootJenkinsApplicationTests.java
+++ src/test/java/com/ms/sj/SpringbootJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringbootJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing..");
+		logger.info("Test case executing second log message..");
 		assertEquals(true, true );
 	}
 
